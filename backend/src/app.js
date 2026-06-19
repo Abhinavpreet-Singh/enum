e7f10813cb8da7bcdd473a6309489ab855850260
Pratham Mittal <prathamv0106@gmail.com>
2026-06-19 13:09:44 +0530

    Added api link to enum for cookies

--- backend/src/app.js
+++ backend/src/app.js
@@ -43,8 +43,8 @@ app.use(
     }),
 )
 
-app.use(express.json({ limit: "16kb" }))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.json({ limit: "1mb" }))
+app.use(express.urlencoded({ extended: true, limit: "1mb" }))
 app.use(cookieParser())
 
 app.use(passport.initialize())
@@ -56,7 +56,7 @@ app.get("/health", (req, res) => {
 console.log("[app] Passport initialized");
 
 app.get("/", (req, res) => {
-	res.send("Hello from the server!")
+	res.send("Hello from the Enum server!")
 })
 
 import userRouter from "./routes/user.route.js"
@@ -69,6 +69,7 @@ import editorialRoute from "./routes/editorial.route.js";
 import simulationRouter from "./routes/simulation.route.js";
 import simulationProgressRouter from "./routes/simulationProgress.route.js";
 import simulationEngineRouter from "./routes/simulationEngine.route.js";
+import compilerRouter from "./routes/compiler.route.js";
 import submissionRouter from "./routes/submission.route.js";
 import complexityRouter from "./routes/complexity.route.js";
 import systemDesignRouter from "./routes/systemDesign.route.js";
@@ -98,6 +99,7 @@ app.use("/api/editorial", editorialRoute);
 app.use("/api/v1/simulations", simulationRouter);
 app.use("/api/v1/simulation-progress", simulationProgressRouter);
 app.use("/api/v1/simulation-engine", simulationEngineRouter);
+app.use("/api/v1/compiler", compilerRouter);
 app.use("/api/v1/submissions", submissionRouter);
 app.use("/api/v1/complexity", complexityRouter);
 app.use("/api/v1/system-design", systemDesignRouter);
